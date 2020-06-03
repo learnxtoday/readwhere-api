773e6ebd47d629501a996e9eb524e0f4c1e6ed9a
Parth Krishna <thenerdsuperuser@gmail.com>
2020-06-03 13:15:13 +0530

    updated with teapot

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
+import requests
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def home_view():
-    return "<h1>Welcome to Geeks for Geeks</h1>"
+def index():
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return ('<pre>' + r.text + '</pre>')
